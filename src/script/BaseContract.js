ca96663edaa597bbde88427471fa97996d054929
duc-san <duc.nv194023@sis.hust.edu.vn>
2024-06-21 15:59:54 +0700

    add sample app

--- src/script/BaseContract.js
+++ src/script/BaseContract.js
@@ -1,27 +1,49 @@
 'use strict'
 
+const fs = require('fs')
+const path = require('path')
+
+const { ethers } = require('ethers')
+
 const Wallet = require('./Wallet.js')
 
 class BaseContract {
-  constructor({
+  constructor ({
     wallet,
   }) {
     this.wallet = wallet
   }
 
-  static create({
+  static create ({
     wallet = Wallet.create()
   } = {}) {
     return new this({ wallet }) 
   }  
   
-  
-  generateContractABI() {
-    throw new Error('need to be inherited')
+  createFilePath({
+    fileName
+  }) {
+    return path.join(__dirname, '../static',fileName)
   }
 
-  createContract() {
-    throw new Error('need to be inherited')
+  generateContractABI ({
+    fileName
+  }) {
+    const filePath = this.createFilePath({
+      fileName
+    })
+
+    const obj = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+    return obj.abi
+  }
+
+  createContract ({
+    contractAddress,
+    contractABI,
+    wallet
+  }) {
+    return new ethers.Contract(contractAddress, contractABI, wallet)
   }
 }
 
--- src/script/SimpleDexContract.js
+++ src/script/SimpleDexContract.js
@@ -1,34 +1,43 @@
 'use strict'
 
-const fs = require('fs')
-
 const { ethers } = require('ethers')
 
 const config = require('../configs/config.js')
 const BaseContract = require('./BaseContract.js')
 
 class SimpleDexContract extends BaseContract {
-  generateContractABI() {
-    const obj = JSON.parse(fs.readFileSync('src/static/SimpleDex.json', 'utf8'));
-
-    return obj.abi
-  }
-
-  async createContract() {
-    return new ethers.Contract(config.simpleDexContractAddress, this.generateContractABI(), this.wallet)
-  }
-
   async createPair() {
-    const simpleDexContract = await this.createContract()
-      const tokenA = config.ducTokenAddress
-      const tokenB = config.hieuTokenAddress
-      const amountADesired = ethers.toBigInt(500000) 
-      const amountBDesired = ethers.toBigInt(250000)
-      const to = config.metamaskAddress
+    const tokenAAddress = config.ducTokenAddress
+    const tokenBAddress = config.hieuTokenAddress
+    const simpleDexContractAddress = config.simpleDexContractAddress
+    const simpleDexContract = this.createContract({
+      contractAddress: simpleDexContractAddress,
+      contractABI: this.generateContractABI({ fileName: 'SimpleDex.json'}),
+      wallet: this.wallet,
+    })
+
+    const ducTokenContract = this.createContract({
+      contractAddress: tokenAAddress,
+      contractABI: this.generateContractABI({ fileName: 'MockERC20.json'}),
+      wallet: this.wallet,
+    })
+
+    const hieuTokenContract = this.createContract({
+      contractAddress: tokenBAddress,
+      contractABI: this.generateContractABI({ fileName: 'MockERC20.json'}),
+      wallet: this.wallet,
+    })
+
+    await ducTokenContract.approve(simpleDexContractAddress,1000000)
+    await hieuTokenContract.approve(simpleDexContractAddress,500000)
+
+    const amountADesired = ethers.toBigInt(500000) 
+    const amountBDesired = ethers.toBigInt(250000)
+    const to = config.metamaskAddress
 
     const tx = await simpleDexContract.addLiquidity(
-      tokenA,
-      tokenB,
+      tokenAAddress,
+      tokenBAddress,
       amountADesired,
       amountBDesired,
       to
